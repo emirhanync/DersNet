ff2c0d72326fff7e7f4bf7d1b79e75cd5bb2b415
emirhanync <yonacemirhan@gmail.com>
2024-04-26 13:58:30 +0300

    öğrenci sayfası,öretmen sayfası,admin sayfası

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -23,12 +24,16 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
@@ -39,10 +44,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.airbnb.android:lottie:6.4.0")
     implementation ("androidx.core:core-splashscreen:1.0.0")
+    implementation ("com.github.ibrahimsn98:SmoothBottomBar:1.7.9")
 }
--- app/src/main/java/com/example/otomat/Secim.kt
+++ app/src/main/java/com/example/otomat/Secim.kt
@@ -1,47 +1,67 @@
 package com.example.otomat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 
 
 class Secim : AppCompatActivity() {
 
-    lateinit var logIn:TextView
-    lateinit var singUp:TextView
-    lateinit var logInLayout:LinearLayout
-    lateinit var singUpLayout:LinearLayout
+    lateinit var ogrenci: TextView
+    lateinit var ogretmen: TextView
+    lateinit var logInLayout: LinearLayout
+    lateinit var singUpLayout: LinearLayout
 
+    lateinit var girisYap: Button
+    lateinit var girisogretmen: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secim)
 
-        logIn=findViewById(R.id.logIn)
-        singUp=findViewById(R.id.singUp)
-        logInLayout=findViewById(R.id.logInLayout)
-        singUpLayout=findViewById(R.id.singUpLayout)
+        ogrenci = findViewById(R.id.logIn)
+        ogretmen = findViewById(R.id.singUp)
+        logInLayout = findViewById(R.id.logInLayout)
+        singUpLayout = findViewById(R.id.singUpLayout)
 
-        singUp.setOnClickListener {
-            singUp.background = resources.getDrawable(R.drawable.switch_trcks,null)
-            singUp.setTextColor(resources.getColor(R.color.textColor,null))
-            logIn.background = null
+        girisYap = findViewById(R.id.girisYapBtnId)
+        girisogretmen = findViewById(R.id.girisogretmenYapBtnId)
+
+        ogretmen.setOnClickListener {
+            ogretmen.background = resources.getDrawable(R.drawable.switch_trcks, null)
+            ogretmen.setTextColor(resources.getColor(R.color.textColor, null))
+            ogrenci.background = null
+            girisYap.visibility = View.GONE
+            girisogretmen.visibility = View.VISIBLE
             singUpLayout.visibility = View.VISIBLE
             logInLayout.visibility = View.GONE
-            logIn.setTextColor(resources.getColor(R.color.dersnet_red,null))
+            ogrenci.setTextColor(resources.getColor(R.color.dersnet_red, null))
         }
-        logIn.setOnClickListener {
-            singUp.background = null
-            singUp.setTextColor(resources.getColor(R.color.dersnet_red,null))
-            logIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+        ogrenci.setOnClickListener {
+            ogretmen.background = null
+            ogretmen.setTextColor(resources.getColor(R.color.dersnet_red, null))
+            ogrenci.background = resources.getDrawable(R.drawable.switch_trcks, null)
+            girisYap.visibility = View.VISIBLE
+            girisogretmen.visibility = View.GONE
             singUpLayout.visibility = View.GONE
             logInLayout.visibility = View.VISIBLE
-            logIn.setTextColor(resources.getColor(R.color.textColor,null))
+            ogrenci.setTextColor(resources.getColor(R.color.textColor, null))
         }
-//        singIn.setOnClickListener {
-//           startActivity(Intent(this@secim,NewActivity::class.java))
-//        }
+
+
+        girisYap.setOnClickListener {
+            var intent = Intent(this, Ogrenci_safya::class.java)
+            startActivity(intent)
+            finish()
+        }
+        girisogretmen.setOnClickListener {
+
+
+        }
+
     }
 }
